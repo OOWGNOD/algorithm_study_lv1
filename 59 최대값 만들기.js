ae83a12fbff512ae83ecdef2b615af92f96be1ef
OOWGNOD <dongwoozzik@naver.com>
2022-12-10 08:00:00 +0000

    1

---
+++ 59 최대값 만들기.js
@@ -0,0 +1,58 @@
+// 최댓값 만들기 (2)
+// 문제 설명
+// 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소 중 두 개를 곱해 만들 수 있는 최댓값을 return하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// -10,000 ≤ numbers의 원소 ≤ 10,000
+// 2 ≤ numbers 의 길이 ≤ 100
+// 입출력 예
+// numbers	result
+// [1, 2, -3, 4, -5]	15
+// [0, -31, 24, 10, 1, 9]	240
+// [10, 20, 30, 5, 5, 20, 5]	600
+// 입출력 예 설명
+// 입출력 예 #1
+
+// 두 수의 곱중 최댓값은 -3 * -5 = 15 입니다.
+// 입출력 예 #2
+
+// 두 수의 곱중 최댓값은 10 * 24 = 240 입니다.
+// 입출력 예 #3
+
+// 두 수의 곱중 최댓값은 20 * 30 = 600 입니다.
+function solution(numbers) {
+    var answer = 0;
+    const posArr = [...numbers].sort((a, b) => b - a);
+    const negArr = [...numbers].sort((a, b) => a - b);
+    if (
+        Math.sign(posArr[1]) !== 0 &&
+        Math.sign(posArr[0]) !== 0 &&
+        Math.sign(negArr[1]) !== 0 &&
+        Math.sign(negArr[0]) !== 0 &&
+        Math.sign(negArr[0]) === -1 &&
+        Math.sign(negArr[1]) === -1 &&
+        negArr[0] * negArr[1] > posArr[0] * posArr[1]
+    ) {
+        return (answer = negArr[0] * negArr[1]);
+    } else if (
+        Math.sign(posArr[1]) !== 0 &&
+        Math.sign(posArr[0]) !== 0 &&
+        Math.sign(negArr[1]) !== 0 &&
+        Math.sign(negArr[0]) !== 0 &&
+        Math.sign(negArr[0]) === -1 &&
+        Math.sign(negArr[1]) === 1
+    ) {
+        return (answer = posArr[0] * posArr[1]);
+    } else if (
+        Math.sign(posArr[1]) !== 0 &&
+        Math.sign(posArr[0]) !== 0 &&
+        Math.sign(negArr[1]) !== 0 &&
+        Math.sign(negArr[0]) !== 0 &&
+        Math.sign(negArr[0]) === 1
+    ) {
+        return (answer = posArr[0] * posArr[1]);
+    } else if (Math.sign(posArr[1]) === 0 || Math.sign(posArr[0]) === 0) {
+        return (answer = 0);
+    } else return (answer = posArr[0] * posArr[1]);
+    return answer;
+}
